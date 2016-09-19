4debf07d595f483fc268db2853af28d3db1426f6
bromano <benjamin.romano@outlook.com>
2016-09-19 14:06:40 -0500

    Cleaned up code

--- content.js
+++ content.js
@@ -6,29 +6,21 @@
 */
 
 //grab all elements on the page
-var elements = document.getElementsByTagName('*');
+let elements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        //if the nodeType is a text node
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            
-            // TODO: Replace Ben with image
-            // TODO: Only match the word Ben, not any sequence of characters 'B' 'e' 'n'
-            var replacedText = text.replace(/Ben/g, 'Bren');
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-
-            replacedText = text.replace(/ben/g, 'bren');
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
+for (let element of elements) {
+    for (let node of element.childNodes) {
+        // Ignore if nodeType is not text
+        if (node.nodeType !== 3) {
+            continue;
         }
+
+        let text = node.nodeValue;
+
+        // TODO: Replace Ben with image
+        let updatedText = text.replace(/Ben/g, 'Bren');
+        updatedText = updatedText.replace(/ben/g, 'bren');
+
+        node.nodeValue = updatedText;
     }
 }
