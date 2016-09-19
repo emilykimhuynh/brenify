0b587b8161ffa97e56fbbd523e9a95295bc07824
Bcpoole <brandoncpoole@gmail.com>
2016-09-19 15:49:03 -0500

    Commented out replace loop as it was breaking sites

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ###Changes:
 
+* *Version 0.3.1:* Removed async replace loop since it was breaking data loading
 * *Version 0.3:* Handles async loading data for first 10 seconds (don't feel like having an eternal loop)
 * *Version 0.2:* Handles lower case "ben"
 * *Version 0.1:* Replace all instances of "Ben" with "Bren".
@@ -9,3 +10,4 @@
 ###Features to Implement:
 
 - option for brenmoji replacement
+- handle async data loading
--- content.js
+++ content.js
@@ -1,19 +1,12 @@
-/*
-*
-* Ripped from here:
-* https://9to5google.com/2015/06/14/how-to-make-a-chrome-extensions/
-*
-*/
-
 brenify();
 
 //handle async data loading for 10 seconds
-(function myLoop (i) {          
+/*(function myLoop (i) {          
    setTimeout(function () {
        brenify();             
       if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
    }, 1000)
-})(10);                        //  pass the number of iterations as an argument
+})(10);                        //  pass the number of iterations as an argument*/
 
 function brenify() {
     document.body.innerHTML = document.body.innerHTML.replace(/Ben/g, 'Bren').replace(/ben/g, 'bren');
