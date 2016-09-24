d97c4d5898e680f867e1ea8e9051aa231f127e0d
JonathanMerklin <jonathanmerklin@gmail.com>
2016-09-23 18:20:48 -0700

    Assorted fixes e.g. Unicode. Needs fixing and optimizing.
    Unicode support, at a cost. Figuring out ways to optimize this since it runs in O(|b||r||e||n|) when a single regEx change will make it O(|b||e||n|) where r is the r-bitrary HTML we're brenifying, and b,e,n are the sets of letters determined to be b,e,n-equivalent (respectively).
    
    Do not merge this into master yet; I think being fancy and making an IIFE (I wanted to prevent polluting the global namespace) is not needed and I have noticed that this currently breaks on some pages (besides being slow). I want to investigate more, but the biggest TODO for speed purposes is to break it down into a RegEx. We'll need to improve the RegExp and replacement string generators to just be a single regEx capturing every (b,e,n) combination as well as just have some ridiculous replacement function that determines the replacement string based on essentially every combination of capturing strings. Yuck, sounds like work. I'm going to get this eventually though, I promise I can be bored enough.

--- content.js
+++ content.js
@@ -1,13 +1,98 @@
-brenify();
+"use strict";
+(()=>{
+    /*
+        TODO: handle more obscure Unicode.
+        Currently handled Unicode blocks:
+        - Basic Latin
+        - Latin-1 Supplement
+        - Latin Extended-A
+        - Latin Extended-B
+        - Latin Extended Additional
+        - Enclosed Alphanumerics
 
-//handle async data loading for 10 seconds
-/*(function myLoop (i) {          
-   setTimeout(function () {
-       brenify();             
-      if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
-   }, 1000)
-})(10);                        //  pass the number of iterations as an argument*/
+        see: https://en.wikipedia.org/wiki/List_of_Unicode_characters
+    */
+    const bs = [
+      "b","B","Ⓑ","⒝",
+      "ⓑ","ƀ","Ɓ","Ƃ",
+      "ƃ","Ƅ","ƅ","Ƀ",
+      "Ḃ","ḃ"
+    ];
+    const es = [
+      "e","E","Ⓔ","⒠",
+      "ⓔ","È","É","Ê",
+      "Ë","è","é","ê",
+      "ë","Ē","ē","Ĕ",
+      "ĕ","Ė","ė","Ę",
+      "ę","Ě","ě","Ǝ",
+      "Ə","Ɛ","Ȅ","ȅ",
+      "Ȇ","ȇ","Ȩ","ȩ",
+      "Ɇ","ɇ"
+    ];
+    const ns = [
+      "n","N","Ⓝ","⒩",
+      "ⓝ","Ñ","ñ","Ń",
+      "ń","Ņ","ņ","Ň",
+      "ň","ŉ","Ŋ","ŋ",
+      "Ɲ","ƞ","Ǹ","ǹ",
+      "ȵ"
+    ];
 
-function brenify() {
-    document.body.innerHTML = document.body.innerHTML.replace(/Ben/g, 'Bren').replace(/ben/g, 'bren');
-}
+    //idea: put each letter in a capturing group and insert an r between them
+    var benExpression = (b,e,n) => new RegExp(`(${b})(${e})(${n})`,"g");
+
+    var brenReplacementString = (b,e,n) => {
+        //until a better heuristic is decided, capitalize the r if B, E, and N are all capitalized.
+        //also, match up to other special cases e.g. enclosed alphanumerics
+        let ben = b+e+n,
+            expressionStr = "bren"; //safe enough default
+        const capitalbs = [
+          "B","Ⓑ","Ɓ","Ƀ",
+          "Ḃ"
+        ];
+        const capitales = [
+          "E","Ⓔ","È","É",
+          "Ê","Ë","Ē","Ĕ",
+          "Ė","Ę","Ě","Ǝ",
+          "Ɛ","Ȅ","Ȇ","Ȩ",
+          "Ɇ"
+        ];
+        const capitalns = [
+          "N","Ⓝ","Ñ","Ń",
+          "Ņ","Ň","Ɲ","Ǹ"
+        ];
+        if(ben==="ⒷⒺⓃ") {
+            expressionStr = "ⒷⓇⒺⓃ";
+        }
+        else if(ben==="⒝⒠⒩") {
+            expressionStr = "⒝⒭⒠⒩";
+        }
+        else if(ben==="ⓑⓔⓝ") {
+            expressionStr = "ⓑⓡⓔⓝ";
+        }
+        else if(capitalbs.indexOf(b) > -1 && capitales.indexOf(e) > -1 && capitalns.indexOf(n) > -1) {
+            //capitalize the R
+            expressionStr = b+"R"+e+n;
+        }
+        else {
+            expressionStr = b+"r"+e+n;
+        }
+        return expressionStr;
+    };
+
+
+    var brenify = () => {
+        let newHTML = document.body.innerHTML; //use temp variable so we can make DOM updates all at once
+        /* TODO: build a single complex regexp and only search through HTML once for effeciency */
+        for(let b of bs) {
+            for(let e of es) {
+                for(let n of ns) {
+                    newHTML = newHTML.replace(benExpression(b,e,n),brenReplacementString(b,e,n));
+                }
+            }
+        }
+        document.body.innerHTML = newHTML;
+    }
+
+    brenify();
+})();
