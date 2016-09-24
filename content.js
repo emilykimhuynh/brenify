31ca33b3a4f8734809b741a81baa29a96300f4a5
JonathanMerklin <jonathanmerklin@gmail.com>
2016-09-23 17:06:15 -0700

    Extend Unicode support and replace in one RegExp. v0.4

--- content.js
+++ content.js
@@ -1,13 +1,105 @@
-brenify();
+"use strict";
+(()=>{
+    /*
+        TODO: handle more obscure Unicode.
+        Currently handled Unicode blocks:
+        - Basic Latin
+        - Latin-1 Supplement
+        - Latin Extended-A
+        - Latin Extended-B
+        - Latin Extended Additional
+        - Enclosed Alphanumerics
 
-//handle async data loading for 10 seconds
-/*(function myLoop (i) {          
-   setTimeout(function () {
-       brenify();             
-      if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
-   }, 1000)
-})(10);                        //  pass the number of iterations as an argument*/
+        see: https://en.wikipedia.org/wiki/List_of_Unicode_characters
+    */
+    const bs = [
+        // Basic Latin
+        "b","B",
+        // Enclosed Alphanumerics
+        "Ⓑ","⒝", "ⓑ",
+        // Latin Extended-B
+        "ƀ","Ɓ","Ƃ","ƃ","Ƅ","ƅ","Ƀ",
+        // Latin Extended Additional
+        "Ḃ","ḃ"
+    ];
+    const es = [
+        // Basic Latin
+        "e","E",
+        // Enclosed Alphanumerics
+        "Ⓔ","⒠","ⓔ",
+        // Latin-1 Supplement
+        "È","É","Ê","Ë","è","é","ê","ë",
+        // Latin Extended-A
+        "Ē","ē","Ĕ","ĕ","Ė","ė","Ę","ę","Ě","ě",
+        // Latin Extended-B
+        "Ǝ","Ə","Ɛ","ǝ","Ȅ","ȅ","Ȇ","ȇ","Ȩ","ȩ","Ɇ","ɇ"
+    ];
+    const ns = [
+        // Basic Latin
+        "n","N",
+        // Enclosed Alphanumerics
+        "Ⓝ","⒩","ⓝ",
+        // Latin-1 Supplement
+        "Ñ","ñ",
+        // Latin Extended-A
+        "Ń","ń","Ņ","ņ","Ň","ň","ŉ","Ŋ","ŋ",
+        // Latin Extended-B
+        "Ɲ","ƞ","Ǹ","ǹ","Ƞ","ȵ"
+  ]; //
+    var brenReplacementString = (b,e,n) => {
+        //until a better heuristic is decided, capitalize the r if B, E, and N are all capitalized.
+        //also, match up to other special cases e.g. enclosed alphanumerics
+        let ben = b+e+n,
+            expressionStr = "bren"; //safe enough default
+        const capitalbs = [
+          "B","Ⓑ","Ɓ","Ƀ",
+          "Ḃ"
+        ];
+        const capitales = [
+          "E","Ⓔ","È","É",
+          "Ê","Ë","Ē","Ĕ",
+          "Ė","Ę","Ě","Ǝ",
+          "Ɛ","Ȅ","Ȇ","Ȩ",
+          "Ɇ"
+        ];
+        const capitalns = [
+          "N","Ⓝ","Ñ","Ń",
+          "Ņ","Ň","Ɲ","Ǹ"
+        ];
+        if(ben==="ⒷⒺⓃ") {
+            expressionStr = "ⒷⓇⒺⓃ";
+        }
+        else if(ben==="⒝⒠⒩") {
+            expressionStr = "⒝⒭⒠⒩";
+        }
+        else if(ben==="ⓑⓔⓝ") {
+            expressionStr = "ⓑⓡⓔⓝ";
+        }
+        else if(capitalbs.indexOf(b) > -1 && capitales.indexOf(e) > -1 && capitalns.indexOf(n) > -1) {
+            //capitalize the R
+            expressionStr = b+"R"+e+n;
+        }
+        else {
+            expressionStr = b+"r"+e+n;
+        }
+        return expressionStr;
+    };
 
-function brenify() {
-    document.body.innerHTML = document.body.innerHTML.replace(/Ben/g, 'Bren').replace(/ben/g, 'bren');
-}
+    /* Main function */
+
+    var brenify = () => {
+        let newHTML = document.body.innerHTML; //use temp variable so we can make DOM updates all at once
+        let benMap = {};
+        for(let b of bs) {
+            for(let e of es) {
+                for(let n of ns) {
+                    benMap[b+e+n] = brenReplacementString(b,e,n);
+                }
+            }
+        }
+        newHTML = newHTML.replace(new RegExp(Object.keys(benMap).join("|"),"g"), matched => benMap[matched]);
+        document.body.innerHTML = newHTML;
+    };
+
+    brenify();
+})();
